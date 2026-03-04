7ee71f9fbe2b0b0918d10bf3a1bae1b6aeb881cb
Vic Cherubini <vcherubini@gmail.com>
2026-03-04 17:42:23 -0600

    wip

--- tests/Client/Poppler/PopplerExtractorClientTest.php
+++ tests/Client/Poppler/PopplerExtractorClientTest.php
@@ -8,8 +8,10 @@ use OneToMany\PdfPack\Client\Poppler\PopplerExtractorClient;
 use OneToMany\PdfPack\Contract\Enum\OutputType;
 use OneToMany\PdfPack\Contract\Response\ExtractedDataResponseInterface;
 use OneToMany\PdfPack\Exception\InvalidArgumentException;
+use OneToMany\PdfPack\Request\ExtractRequest;
 use OneToMany\PdfPack\Request\ExtractTextRequest;
 use OneToMany\PdfPack\Request\ReadRequest;
+use OneToMany\PdfPack\Response\ExtractResponse;
 use PHPUnit\Framework\Attributes\DataProvider;
 use PHPUnit\Framework\Attributes\Group;
 use PHPUnit\Framework\Attributes\Large;
@@ -28,17 +30,15 @@ use function random_int;
 #[Group('PopplerTests')]
 final class PopplerExtractorClientTest extends TestCase
 {
-    public function testReadingMetadataRequiresValidPdfInfoBinary(): void
+    public function testReadingFileRequiresValidPdfInfoBinary(): void
     {
-        $pdfInfoBinary = 'invalid_pdfinfo_binary';
-
         $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('The binary "'.$pdfInfoBinary.'" could not be found.');
+        $this->expectExceptionMessage('The binary "invalid_pdfinfo_binary" could not be found.');
 
-        new PopplerExtractorClient(pdfInfoBinary: $pdfInfoBinary)->read(new ReadRequest(__FILE__));
+        new PopplerExtractorClient(pdfInfoBinary: 'invalid_pdfinfo_binary')->read(new ReadRequest(__FILE__));
     }
 
-    public function testReadingMetadataRequiresValidPdfFile(): void
+    public function testReadingFileRequiresValidPdfFile(): void
     {
         $this->expectException(ReadingFileFailedException::class);
         $this->expectExceptionMessageMatches('/May not be a PDF file/');
@@ -46,28 +46,26 @@ final class PopplerExtractorClientTest extends TestCase
         new PopplerExtractorClient()->read(new ReadRequest(__FILE__));
     }
 
-    #[DataProvider('providerReadingMetadata')]
-    public function testReadingMetadata(string $filePath, int $pages): void
+    #[DataProvider('providerPathAndPages')]
+    public function testReadingFile(string $path, int $pages): void
     {
-        $client = new PopplerExtractorClient();
+        $response = new PopplerExtractorClient()->read(...[
+            'request' => new ReadRequest($path),
+        ]);
 
-        $metadata = $client->read(
-            new ReadRequest($filePath),
-        );
-
-        $this->assertEquals($pages, $metadata->getPages());
+        $this->assertEquals($pages, $response->getPages());
     }
 
     /**
-     * @return list<list<int|string|OutputType>>
+     * @return list<list<int|non-empty-string|OutputType>>
      */
-    public static function providerReadingMetadata(): array
+    public static function providerPathAndPages(): array
     {
         $provider = [
-            [__DIR__.'/../files/pages-1.pdf', 1],
-            [__DIR__.'/../files/pages-2.pdf', 2],
-            [__DIR__.'/../files/pages-3.pdf', 3],
-            [__DIR__.'/../files/pages-4.pdf', 4],
+            [__DIR__.'/../../data/pages-1.pdf', 1],
+            [__DIR__.'/../../data/pages-2.pdf', 2],
+            [__DIR__.'/../../data/pages-3.pdf', 3],
+            [__DIR__.'/../../data/pages-4.pdf', 4],
         ];
 
         return $provider;
@@ -75,98 +73,92 @@ final class PopplerExtractorClientTest extends TestCase
 
     public function testExtractingImageDataRequiresValidPdfToPpmBinary(): void
     {
-        $pdfToPpmBinary = 'invalid_pdftoppm_binary';
+        $request = new ExtractRequest(__DIR__.'/../../data/pages-1.pdf')->asJpegOutput();
 
         $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('The binary "'.$pdfToPpmBinary.'" could not be found.');
+        $this->expectExceptionMessage('The binary "invalid_pdftoppm_binary" could not be found.');
 
-        new PopplerExtractorClient(pdfToPpmBinary: $pdfToPpmBinary)->extract(new ExtractRequest(__FILE__))->current();
+        new PopplerExtractorClient(pdfToPpmBinary: 'invalid_pdftoppm_binary')->extract($request)->current();
     }
 
     public function testExtractingTextDataRequiresValidPdfToTextBinary(): void
     {
-        $pdfToTextBinary = 'invalid_pdftotext_binary';
+        $request = new ExtractRequest(__DIR__.'/../../data/pages-1.pdf')->asTextOutput();
 
         $this->expectException(InvalidArgumentException::class);
-        $this->expectExceptionMessage('The binary "'.$pdfToTextBinary.'" could not be found.');
+        $this->expectExceptionMessage('The binary "invalid_pdftotext_binary" could not be found.');
 
-        new PopplerExtractorClient(pdfToTextBinary: $pdfToTextBinary)->extract(new ExtractTextRequest(__FILE__))->current();
+        new PopplerExtractorClient(pdfToTextBinary: 'invalid_pdftotext_binary')->extract($request)->current();
     }
 
     public function testExtractingDataRequiresValidPdfFile(): void
     {
+        $request = new ExtractRequest(__FILE__)->toPage(1);
+
         $this->expectException(ExtractingDataFailedException::class);
         $this->expectExceptionMessageMatches('/May not be a PDF file/');
 
-        new PopplerExtractorClient()->extract(new ExtractRequest(__FILE__))->current();
+        new PopplerExtractorClient()->extract($request)->current();
     }
 
     public function testExtractingDataRequiresValidPage(): void
     {
-        $page = random_int(2, 100);
+        $request = new ExtractRequest(__DIR__.'/../../data/pages-1.pdf')->fromPage(2)->toPage(2);
 
         $this->expectException(ExtractingDataFailedException::class);
         $this->expectExceptionMessageMatches('/Wrong page range given/');
 
-        new PopplerExtractorClient()->extract(new ExtractRequest(__DIR__.'/../files/pages-1.pdf', $page, $page))->current();
+        new PopplerExtractorClient()->extract($request)->current();
     }
 
-    #[DataProvider('providerExtractingDataRange')]
-    public function testExtractingDataRange(
-        string $filePath,
-        int $firstPage,
-        ?int $lastPage,
-        int $responseCount,
-    ): void {
-        $client = new PopplerExtractorClient();
+    #[DataProvider('providerPathFirstPageLastPageAndResponseCount')]
+    public function testExtractingDataRange(string $path, int $firstPage, ?int $lastPage, int $responseCount): void
+    {
+        $request = new ExtractRequest($path, $firstPage, $lastPage);
 
-        $request = new ExtractRequest(
-            $filePath, $firstPage, $lastPage,
-        );
-
-        /** @var list<ExtractedDataResponseInterface> $responses */
-        $responses = iterator_to_array($client->extract($request));
+        /** @var list<ExtractResponse> $responses */
+        $responses = iterator_to_array(new PopplerExtractorClient()->extract($request));
 
         $this->assertCount($responseCount, $responses);
-        $this->assertContainsOnlyInstancesOf(ExtractedDataResponseInterface::class, $responses); // @phpstan-ignore-line
+        $this->assertContainsOnlyInstancesOf(ExtractResponse::class, $responses); // @phpstan-ignore-line
     }
 
     /**
      * @return list<list<int|string|null>>
      */
-    public static function providerExtractingDataRange(): array
+    public static function providerPathFirstPageLastPageAndResponseCount(): array
     {
         $provider = [
-            [__DIR__.'/../files/pages-1.pdf', 1, 1, 1],
-            [__DIR__.'/../files/pages-1.pdf', 1, null, 1],
-            [__DIR__.'/../files/pages-2.pdf', 1, 1, 1],
-            [__DIR__.'/../files/pages-2.pdf', 1, 2, 2],
-            [__DIR__.'/../files/pages-2.pdf', 1, null, 2],
-            [__DIR__.'/../files/pages-2.pdf', 2, 2, 1],
-            [__DIR__.'/../files/pages-2.pdf', 2, null, 1],
-            [__DIR__.'/../files/pages-3.pdf', 1, 1, 1],
-            [__DIR__.'/../files/pages-3.pdf', 1, 2, 2],
-            [__DIR__.'/../files/pages-3.pdf', 1, 3, 3],
-            [__DIR__.'/../files/pages-3.pdf', 1, null, 3],
-            [__DIR__.'/../files/pages-3.pdf', 2, 2, 1],
-            [__DIR__.'/../files/pages-3.pdf', 2, 3, 2],
-            [__DIR__.'/../files/pages-3.pdf', 2, null, 2],
-            [__DIR__.'/../files/pages-3.pdf', 3, 3, 1],
-            [__DIR__.'/../files/pages-3.pdf', 3, null, 1],
-            [__DIR__.'/../files/pages-4.pdf', 1, 1, 1],
-            [__DIR__.'/../files/pages-4.pdf', 1, 2, 2],
-            [__DIR__.'/../files/pages-4.pdf', 1, 3, 3],
-            [__DIR__.'/../files/pages-4.pdf', 1, 4, 4],
-            [__DIR__.'/../files/pages-4.pdf', 1, null, 4],
-            [__DIR__.'/../files/pages-4.pdf', 2, 2, 1],
-            [__DIR__.'/../files/pages-4.pdf', 2, 3, 2],
-            [__DIR__.'/../files/pages-4.pdf', 2, 4, 3],
-            [__DIR__.'/../files/pages-4.pdf', 2, null, 3],
-            [__DIR__.'/../files/pages-4.pdf', 3, 3, 1],
-            [__DIR__.'/../files/pages-4.pdf', 3, 4, 2],
-            [__DIR__.'/../files/pages-4.pdf', 3, null, 2],
-            [__DIR__.'/../files/pages-4.pdf', 4, 4, 1],
-            [__DIR__.'/../files/pages-4.pdf', 4, null, 1],
+            [__DIR__.'/../../data/pages-1.pdf', 1, 1, 1],
+            [__DIR__.'/../../data/pages-1.pdf', 1, null, 1],
+            [__DIR__.'/../../data/pages-2.pdf', 1, 1, 1],
+            [__DIR__.'/../../data/pages-2.pdf', 1, 2, 2],
+            [__DIR__.'/../../data/pages-2.pdf', 1, null, 2],
+            [__DIR__.'/../../data/pages-2.pdf', 2, 2, 1],
+            [__DIR__.'/../../data/pages-2.pdf', 2, null, 1],
+            [__DIR__.'/../../data/pages-3.pdf', 1, 1, 1],
+            [__DIR__.'/../../data/pages-3.pdf', 1, 2, 2],
+            [__DIR__.'/../../data/pages-3.pdf', 1, 3, 3],
+            [__DIR__.'/../../data/pages-3.pdf', 1, null, 3],
+            [__DIR__.'/../../data/pages-3.pdf', 2, 2, 1],
+            [__DIR__.'/../../data/pages-3.pdf', 2, 3, 2],
+            [__DIR__.'/../../data/pages-3.pdf', 2, null, 2],
+            [__DIR__.'/../../data/pages-3.pdf', 3, 3, 1],
+            [__DIR__.'/../../data/pages-3.pdf', 3, null, 1],
+            [__DIR__.'/../../data/pages-4.pdf', 1, 1, 1],
+            [__DIR__.'/../../data/pages-4.pdf', 1, 2, 2],
+            [__DIR__.'/../../data/pages-4.pdf', 1, 3, 3],
+            [__DIR__.'/../../data/pages-4.pdf', 1, 4, 4],
+            [__DIR__.'/../../data/pages-4.pdf', 1, null, 4],
+            [__DIR__.'/../../data/pages-4.pdf', 2, 2, 1],
+            [__DIR__.'/../../data/pages-4.pdf', 2, 3, 2],
+            [__DIR__.'/../../data/pages-4.pdf', 2, 4, 3],
+            [__DIR__.'/../../data/pages-4.pdf', 2, null, 3],
+            [__DIR__.'/../../data/pages-4.pdf', 3, 3, 1],
+            [__DIR__.'/../../data/pages-4.pdf', 3, 4, 2],
+            [__DIR__.'/../../data/pages-4.pdf', 3, null, 2],
+            [__DIR__.'/../../data/pages-4.pdf', 4, 4, 1],
+            [__DIR__.'/../../data/pages-4.pdf', 4, null, 1],
         ];
 
         return $provider;
@@ -198,11 +190,11 @@ final class PopplerExtractorClientTest extends TestCase
     public static function providerExtractingTextData(): array
     {
         $provider = [
-            [__DIR__.'/../files/pages-1.pdf', 1, ''],
-            [__DIR__.'/../files/pages-2.pdf', 1, 'Amazon Simple Storage Service'],
-            [__DIR__.'/../files/pages-2.pdf', 2, 'Storage Service: API Reference'],
-            [__DIR__.'/../files/pages-3.pdf', 3, 'Learn more about the AWS CLI'],
-            [__DIR__.'/../files/pages-4.pdf', 4, 'API Version 2006-03-01 iv'],
+            [__DIR__.'/../../data/pages-1.pdf', 1, ''],
+            [__DIR__.'/../../data/pages-2.pdf', 1, 'Amazon Simple Storage Service'],
+            [__DIR__.'/../../data/pages-2.pdf', 2, 'Storage Service: API Reference'],
+            [__DIR__.'/../../data/pages-3.pdf', 3, 'Learn more about the AWS CLI'],
+            [__DIR__.'/../../data/pages-4.pdf', 4, 'API Version 2006-03-01 iv'],
         ];
 
         return $provider;
@@ -245,15 +237,15 @@ final class PopplerExtractorClientTest extends TestCase
     public static function providerExtractingImageData(): array
     {
         $provider = [
-            [__DIR__.'/../files/pages-1.pdf', 1, OutputType::Jpeg, 48, '832bfdfd9a01a3087f765b54684347f4'],
-            [__DIR__.'/../files/pages-1.pdf', 1, OutputType::Jpeg, 72, '8d56f696328dfaf06c963e1179456d25'],
-            [__DIR__.'/../files/pages-1.pdf', 1, OutputType::Jpeg, 150, '080f873a6769b81d38f877d511e22a3c'],
-            [__DIR__.'/../files/pages-1.pdf', 1, OutputType::Jpeg, 300, '7573ebf741870bb85c30196013397a55'],
-            [__DIR__.'/../files/pages-1.pdf', 1, OutputType::Png, 48, 'a3b9529090369a93045cefe5b71151d6'],
-            [__DIR__.'/../files/pages-1.pdf', 1, OutputType::Png, 300, '96a6476a69171db7a8ac55848323b219'],
-            [__DIR__.'/../files/pages-2.pdf', 2, OutputType::Jpeg, 72, '3aa7339e59d5991590a14e26a2057002'],
-            [__DIR__.'/../files/pages-3.pdf', 3, OutputType::Jpeg, 72, 'f06ac9888a4da750bb7450f3955e7123'],
-            [__DIR__.'/../files/pages-4.pdf', 4, OutputType::Jpeg, 72, '48c209de64d027ed053a9e931da4ebc0'],
+            [__DIR__.'/../../data/pages-1.pdf', 1, OutputType::Jpeg, 48, '832bfdfd9a01a3087f765b54684347f4'],
+            [__DIR__.'/../../data/pages-1.pdf', 1, OutputType::Jpeg, 72, '8d56f696328dfaf06c963e1179456d25'],
+            [__DIR__.'/../../data/pages-1.pdf', 1, OutputType::Jpeg, 150, '080f873a6769b81d38f877d511e22a3c'],
+            [__DIR__.'/../../data/pages-1.pdf', 1, OutputType::Jpeg, 300, '7573ebf741870bb85c30196013397a55'],
+            [__DIR__.'/../../data/pages-1.pdf', 1, OutputType::Png, 48, 'a3b9529090369a93045cefe5b71151d6'],
+            [__DIR__.'/../../data/pages-1.pdf', 1, OutputType::Png, 300, '96a6476a69171db7a8ac55848323b219'],
+            [__DIR__.'/../../data/pages-2.pdf', 2, OutputType::Jpeg, 72, '3aa7339e59d5991590a14e26a2057002'],
+            [__DIR__.'/../../data/pages-3.pdf', 3, OutputType::Jpeg, 72, 'f06ac9888a4da750bb7450f3955e7123'],
+            [__DIR__.'/../../data/pages-4.pdf', 4, OutputType::Jpeg, 72, '48c209de64d027ed053a9e931da4ebc0'],
         ];
 
         return $provider;
