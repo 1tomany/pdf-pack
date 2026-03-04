186f6e327a223975942cd3d4b1ada970e60f08a5
Vic Cherubini <vcherubini@gmail.com>
2026-03-04 16:38:50 -0600

    wip

--- examples/direct.php
+++ examples/direct.php
@@ -2,31 +2,35 @@
 
 require_once __DIR__.'/../vendor/autoload.php';
 
-use OneToMany\PDFAI\Client\Poppler\PopplerExtractorClient;
-use OneToMany\PDFAI\Contract\Enum\OutputType;
-use OneToMany\PDFAI\Request\ExtractDataRequest;
-use OneToMany\PDFAI\Request\ExtractTextRequest;
-use OneToMany\PDFAI\Request\ReadMetadataRequest;
+use OneToMany\PdfPack\Client\Poppler\PopplerExtractorClient;
+use OneToMany\PdfPack\Request\ExtractRequest;
+use OneToMany\PdfPack\Request\ReadRequest;
 
-$filePath = realpath(__DIR__.'/../tests/Client/files/pages-4.pdf');
+/** @var non-empty-string $path */
+$path = realpath(__DIR__.'/data/pages-4.pdf');
+assert(is_file($path) && is_readable($path));
 
 $client = new PopplerExtractorClient();
 
-$metadata = $client->readMetadata(new ReadMetadataRequest($filePath));
-printf("The PDF '%s' has %d page(s).\n", $filePath, $metadata->getPages());
+$response = $client->read(new ReadRequest($path));
+printf("The PDF '%s' has %d page(s).\n", $path, $response->getPages());
 
 // Rasterize all pages as 150 DPI JPEGs
-$request = new ExtractDataRequest($filePath, 1, null, OutputType::Jpg, 150);
+$extractRequest = new ExtractRequest($path)
+    ->fromPage(1)
+    ->asJpegOutput()
+    ->atResolution(150);
 
-foreach ($client->extractData($request) as $image) {
-    // $image->getData() or $image->toDataUri()
+foreach ($client->extract($extractRequest) as $image) {
     printf("MD5: %s\n", md5($image->getData()));
 }
 
 // Extract text from pages 3 and 4
-$request = new ExtractTextRequest($filePath, 3, 4);
+$extractRequest = new ExtractRequest($path)
+    ->fromPage(3)
+    ->toPage(4)
+    ->asTextOutput();
 
-foreach ($client->extractData($request) as $text) {
-    // $text->getData()
+foreach ($client->extract($extractRequest) as $text) {
     printf("Length: %d\n", strlen($text->getData()));
 }
